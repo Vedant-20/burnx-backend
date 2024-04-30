74eda99d1dde0f77d073098657d0b15ab6c3ffb0
Vedant <vedantwarjurkar@gmail.com>
2024-04-30 16:02:23 +0530

    models and socket connection done

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
+import { app, server } from "./socket/socket.js";
+import connectDB from "./db/connectDB.js";
+
+dotenv.config();
+
+connectDB();
+
+const PORT = process.env.PORT || 8080;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+// Middlewares
+app.use(express.json({ limit: "50mb" })); // To parse JSON data in the req.body
+app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
+app.use(cookieParser());
+
+server.listen(PORT, () =>
+  console.log(`Server started at http://localhost:${PORT}`)
+);
