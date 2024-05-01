e84b08583c1781ff98552478dfeafeeb983d5ec8
Vedant <vedantwarjurkar@gmail.com>
2024-05-01 11:10:02 +0530

    Added controllers and middlewares

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
 import { app, server } from "./socket/socket.js";
 import connectDB from "./db/connectDB.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -18,6 +19,8 @@ cloudinary.config({
 });
 
 // Middlewares
+app.use(cors());
+
 app.use(express.json({ limit: "50mb" })); // To parse JSON data in the req.body
 app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
 app.use(cookieParser());
